3c71f82cf1318702eb87e25bec297d0394929554
mahmudurmahid <m.mahid@icloud.com>
2025-06-29 20:28:15 +0100

    main components added

--- to-do-list-app/src/App.js
+++ to-do-list-app/src/App.js
@@ -1,3 +1,18 @@
 export default function App() {
-  return <div>Hello React</div>;
+  return (
+    <div className="steps">
+      <div className="numbers">
+        <div>1</div>
+        <div>2</div>
+        <div>3</div>
+      </div>
+
+      <p className="message">Message</p>
+
+      <div className="buttons">
+        <button>Previous</button>
+        <button>Next</button>
+      </div>
+    </div>
+  );
 }
