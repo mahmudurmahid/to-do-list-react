23f0ebda255e01bfe61940b29c6b87da6da28c25
mahmudurmahid <m.mahid@icloud.com>
2025-06-29 20:32:30 +0100

    changed number styles

--- to-do-list-app/src/App.js
+++ to-do-list-app/src/App.js
@@ -2,7 +2,7 @@ export default function App() {
   return (
     <div className="steps">
       <div className="numbers">
-        <div>1</div>
+        <div className="active">1</div>
         <div>2</div>
         <div>3</div>
       </div>
